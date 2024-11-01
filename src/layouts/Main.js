b98ff08ca840a48de1145ab8d8dbaf3f35cdad7c
alireza saharkhiz <as.alirezasaharkhiz@gmail.com>
2024-11-01 09:19:43 -0700

    Update Main.js

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <HelmetProvider>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Marius Mercier" defaultTitle="Marius Mercier" defer={false}>
+    <Helmet titleTemplate="%s | Alireza Saharkhiz" defaultTitle="Alireza Saharkhiz" defer={false}>
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Marius Mercier's personal website.",
+  description: "Alireza Saharkhiz's personal website.",
 };
 
 export default Main;
