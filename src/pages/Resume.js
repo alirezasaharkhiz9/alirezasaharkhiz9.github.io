bec7e22cae61b45407d6fc31d5736ed55b1143c3
alireza saharkhiz <as.alirezasaharkhiz@gmail.com>
2024-11-01 09:37:35 -0700

    Update Resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -25,6 +25,20 @@ const sections = {
 
 // const { PUBLIC_URL } = process.env;
 
+const sections = [
+  'Education',
+  'Experience',
+  'Skills',
+  'Courses',
+];
+
+const sections = [
+  'Education',
+  'Experience',
+  'Skills',
+  'Courses',
+];
+
 const Resume = () => (
   <Main
     title="Resume"
@@ -33,9 +47,9 @@ const Resume = () => (
     <article className="post" id="resume">
       <header>
         <div className="title">
-          <h2><Link to="resume">Resume</Link></h2>
+          <h2 data-testid="heading"><Link to="resume">Resume</Link></h2>
           <div className="link-container">
-            {Object.keys(sections).map((sec) => (
+            {sections.map((sec) => (
               <h4 key={sec}>
                 <a href={`#${sec.toLowerCase()}`}>{sec}</a>
               </h4>))}
@@ -53,11 +67,13 @@ const Resume = () => (
         </a>
         .
       </p>
-      {Object.entries(sections).map(([name, Section]) => (
-        <Section key={name} />
-      ))}
+      <Education data={degrees} />
+      <Experience data={positions} />
+      <Skills skills={skills} categories={categories} />
+      <Courses data={courses} />
     </article>
   </Main>
 );
+
 
 export default Resume;
