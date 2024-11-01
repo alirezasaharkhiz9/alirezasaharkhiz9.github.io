6d72440d8d1e59ffd919b398ab3b177654bfe516
alireza saharkhiz <as.alirezasaharkhiz@gmail.com>
2024-11-01 09:27:19 -0700

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,21 +5,22 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Marius Mercier's personal website. Psychology Graduate Student."}
+    description={"Alireza Saharkhiz's personal website. Statistics student."}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2><Link to="/">About this site</Link></h2>
           <p>
-            Academic and Personal website of Marius Mercier.
+            Academic and Personal website of Alireza Saharkhiz.
           </p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+        check out my {' '}
+        <Link to="/resume">resume</Link> {' '}
+        and <Link to="/projects">projects</Link>, {' '}
+        or <Link to="/contact">contact me</Link>.
       </p>
       <p> </p>
     </article>
