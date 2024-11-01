2497f6520796a0cc1a69459316ef15068946464e
alireza saharkhiz <as.alirezasaharkhiz@gmail.com>
2024-11-01 08:41:19 -0700

    Update courses.js

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -1,9 +1,15 @@
 const courses = [
   {
-    title: 'TEST',
-    number: '?',
-    link: '',
-    university: 'ENS',
+    title: 'Machine Learning A-Z™',
+    number: 'Udemy',
+    link: 'https://www.udemy.com/course/machinelearning/?srsltid=AfmBOoqGIBZV3itNW8ZwoOgjW1FxUVGdvkAu4qwHvWpMMVBF8keyHSpt',
+    university: 'udemy',
+  },
+  {
+    title: 'Deep Learning A-Z™',
+    number: 'Udemy',
+    link: 'https://www.udemy.com/course/deeplearning/?srsltid=AfmBOopiqCKVkWV3Lxk1190c_CvVTLOE_TxTvnlpj0EW-2AZ5D_WdJkX',
+    university: 'udemy',
   },
 ];
 
